0f034629ae949e809bfbbc80e6d82ce94ba785f3
Franchely Portorreal <franchely811@gmail.com>
2019-04-28 19:12:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
-function HappyHolidaysTo(name) {
+function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday, name) {
